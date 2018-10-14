efcbb342e54affc12b7f76024174ad9a90428b73
Khairul Ikhwan <mk.ikhwan88@gmail.com>
2018-10-14 08:00:04 +0800

    Revert back to original Node.JS

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var express = require('express')
 var app = express()
-var http = require('http');
-var fs = require('fs');
-
-const PORT=8080; 
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-/*
+
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
@@ -15,16 +11,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-*/
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    //}).listen(PORT);
-	}).listen(app.get('port'));
-});
